494eaa5544403610c83e9ac102de032d57c4dc1d
Your Name <you@example.com>
2024-02-20 11:46:29 +0530

    Updated Assignment3

--- Module3/main.js
+++ Module3/main.js
@@ -3,7 +3,7 @@ let age=25
 //Using if else 
 if(age<=12){
     console.log("child")
-}else if(age<=13 && age<=18){
+}else if(age>=13 && age<=18){
     console.log("teen")
 }else{
     console.log("adult")
@@ -15,7 +15,7 @@ switch(true)
 {
     case age<=12:console.log("child")
         break
-    case age<=18:console.log("teen")
+    case (age>=13 && age<=18):console.log("teen")
         break
     default:console.log("adult")
 }
